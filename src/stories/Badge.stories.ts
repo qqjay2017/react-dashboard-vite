ef0d01cf7832f24b83b5f7ef897f4c7692fba831
huangbo <huangbo@lj-ch.com>
2024-03-25 22:23:28 +0800

    feat: storybook项目初始化

---
+++ src/stories/Badge.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Badge } from "../lib";
+
+const meta: Meta<typeof Badge> = {
+  title: "Components/Badge",
+  component: Badge,
+  // ...
+};
+
+export default meta;
+// type Story = StoryObj<Badge>;
+
+// export const Primary: Story = {
+//   args: {
+//     primary: true,
+//     label: "Click",
+//     background: "red",
+//   },
+// };
